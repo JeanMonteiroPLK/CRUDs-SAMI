05d65f9104e6420d11badb6680a4ca28616a4af6
Jean "Polako" Monteiro <84013209+JeanMonteiroPLK@users.noreply.github.com>
2022-06-07 19:04:05 -0300

    Build obj responsável

--- CRUDUser.js
+++ CRUDUser.js
@@ -6,22 +6,21 @@ var posicao
 var novoUsuario
 var novoEndereco
 var novoID
+var idDoTitular
 
 var id, nome, cpf, senha, endereco
 var cidade, bairro, rua, numero, complemento
 // ----- RESPONSÁVEL -----
-/*
+
 var selectResp = document.querySelector('#responsavel')
 selectResp.addEventListener('change', function () { })
 
 var novoResponsavel
 var novoEnderecoResponsavel
-var novoIDResponsavel
-var idTitular
 
 var idResp, nomeResp, cpfResp, senhaResp, enderecoResp
 var cidadeResp, bairroResp, ruaResp, numeroResp, complementoResp
-*/
+
 var selectAtributosUsuario = document.querySelector('#atributo')
 selectAtributosUsuario.addEventListener('change', function () { })
 
@@ -37,12 +36,24 @@ var ruaInput = document.getElementById("rua")
 var numeroInput = document.getElementById("numero")
 var complementoInput = document.getElementById("complemento")
 
-var idParaBuscar
+var idParaBuscar = document.getElementById("idParaBuscar")
+var nomeParaBuscar = document.getElementById("nomeParaBuscar")
+var cpfParaBuscar = document.getElementById("cpfParaBuscar")
 var idParaDeletar = document.getElementById("idParaDeletar")
 var idParaEditar = document.getElementById("idParaEditar")
 var novoValorEditar = document.getElementById("novoValorEditar")
 
 // ----- INPUTS HTML RESPONSÁVEL -----
+var nomeRespInput
+var cpfRespInput
+var senhaRespInput
+
+var cidadeRespInput
+var bairroRespInput
+var ruaRespInput
+var numeroRespInput
+var complementoRespInput
+
 /*
 var nomeRespInput = document.getElementById("nomeResp")
 var cpfRespInput = document.getElementById("cpfResp")
@@ -72,7 +83,7 @@ function Endereco(cidade, bairro, rua, numero, complemento) {
     this.numero = numero
     this.complemento = complemento
 }
-/*
+
 function Responsavel(idResp, nomeResp, cpfResp, senhaResp, enderecoResp) {
     this.idResp = idResp
     this.nomeResp = nomeResp
@@ -88,7 +99,7 @@ function EnderecoResponsavel(cidadeResp, bairroResp, ruaResp, numeroResp, comple
     this.numeroResp = numeroResp
     this.complementoResp = complementoResp
 }
-*/
+
 
 // ----- CRUD -----
 
@@ -96,12 +107,14 @@ function cadastrarNovoUsuario() {
     validaListaUsuariosDoLocalStorage()
     novoID = gerarIdAleatorio(1, 100)
     id = novoID
-
+    
     novoEndereco = criarObjetoEndereco()
     novoUsuario = criarObjetoUsuario()
+    idDoTitular = novoUsuario.id
 
     if (!ehListaVazia(listaDeUsuarios)) {
-        if (cpfUsuarioEstaCadastrado(novoUsuario.cpf)) {
+        if (existeCpf(novoUsuario.cpf)) {
+            alert(`CPF [${novoUsuario.cpf}] já cadastrado!`)
             return
         }
 
@@ -112,26 +125,31 @@ function cadastrarNovoUsuario() {
         }
     }
 
-    /*
-    if (selectResp.value == 'sim') {
-        idResp = novoUsuario.id
-
-        novoEnderecoResponsavel = criarObjetoEnderecoResponsavel()
-        novoResponsavel = criarObjetoResponsavel()
-
-        if (!ehListaVazia(listaDeResponsaveis)) {
-            if (cpfResponsavelEstaCadastrado(novoResponsavel.cpf)) {
-                return
-            }
-        }
-    }
-    */
-
     listaDeUsuarios.push(novoUsuario)
     localStorage.setItem("usuariosKey", JSON.stringify(listaDeUsuarios))
 
     alert('Novo usuário cadastrado!')
 
+}
+
+function cadastrarNovoResponsavel() {
+    validaListaResponsaveisDoLocalStorage()
+    
+    novoEnderecoResponsavel = criarObjetoEnderecoResponsavel()
+    novoResponsavel = criarObjetoResponsavel(idDoTitular)
+
+    if (!ehListaVazia(listaDeResponsaveis)) {
+        if (existeCpfResponsavel(novoResponsavel.cpf)) {
+            alert(novoResponsavel.cpf)
+            return
+        }
+    }
+
+    listaDeResponsaveis.push(novoResponsavel)
+    localStorage.setItem("responsaveisKey", JSON.stringify(listaDeResponsaveis))
+
+    alert("Novo responsável cadastrado!")
+    location.reload()
 }
 
 function listarTodosUsuariosSalvos() {
@@ -147,6 +165,66 @@ function listarTodosUsuariosSalvos() {
             stringPrint += `<br> ${objetoAtual.id} | ${objetoAtual.nome} | ${objetoAtual.cpf} <br>`
         }
         document.querySelector('#listar').innerHTML = cabecalho + stringPrint
+    }
+}
+
+function buscarUsuarioPorId() {
+    listaDeUsuarios = JSON.parse(localStorage.getItem("usuariosKey"))
+    var usuarioBuscadoPorId
+
+    if (listaDeUsuarios == null || listaDeUsuarios == '') {
+        alert('Não há registros!')
+    } else {
+        if (!ehInputVazio(idParaBuscar.value)) {
+            if (existeId(idParaBuscar.value)) {
+                usuarioBuscadoPorId = listaDeUsuarios[posicao]
+                alert(`ID: ${usuarioBuscadoPorId.id}\nNome: ${usuarioBuscadoPorId.nome}\nCPF: ${usuarioBuscadoPorId.cpf}`)
+            } else {
+                alert(`O seguinte ID não existe: [${idParaBuscar.value}]`)
+            }
+        } else {
+            alert("Campo não pode ser vazio!")
+        }
+    }
+}
+
+function buscarUsuarioPorNome() {
+    listaDeUsuarios = JSON.parse(localStorage.getItem("usuariosKey"))
+    var usuarioBuscadoPorNome
+
+    if (listaDeUsuarios == null || listaDeUsuarios == '') {
+        alert('Não há registros!')
+    } else {
+        if (!ehInputVazio(nomeParaBuscar.value)) {
+            if (existeNome(nomeParaBuscar.value)) {
+                usuarioBuscadoPorNome = listaDeUsuarios[posicao]
+                alert(`ID: ${usuarioBuscadoPorNome.id}\nNome: ${usuarioBuscadoPorNome.nome}\nCPF: ${usuarioBuscadoPorNome.cpf}`)
+            } else {
+                alert(`O seguinte usuário não existe: [${nomeParaBuscar.value}]`)
+            }
+        } else {
+            alert("Campo não pode ser vazio!")
+        }
+    }
+}
+
+function buscarUsuarioPorCpf() {
+    listaDeUsuarios = JSON.parse(localStorage.getItem("usuariosKey"))
+    var usuarioBuscadoPorCpf
+
+    if (listaDeUsuarios == null || listaDeUsuarios == '') {
+        alert('Não há registros!')
+    } else {
+        if (!ehInputVazio(cpfParaBuscar.value)) {
+            if (existeCpf(cpfParaBuscar.value)) {
+                usuarioBuscadoPorCpf = listaDeUsuarios[posicao]
+                alert(`ID: ${usuarioBuscadoPorCpf.id}\nNome: ${usuarioBuscadoPorCpf.nome}\nCPF: ${usuarioBuscadoPorCpf.cpf}`)
+            } else {
+                alert(`O seguinte CPF não existe: [${cpfParaBuscar.value}]`)
+            }
+        } else {
+            alert("Campo não pode ser vazio!")
+        }
     }
 }
 
@@ -180,14 +258,35 @@ function editarUsuario() {
                     listaDeUsuarios[posicao].senha = novoValorEditar.value
                     break
 
-                /* Validar com Lucas sobre como abrir um <select> a partir de uma <option> de outro <select>
-                    case 'endereco':
-                    listaDeUsuarios[posicao].endereco = novoValorEditar.value
-                    break
-                */
+                case 'endereco':
+                    switch (atributoEndereco.value) {
 
+                        case 'cidade':
+                            listaDeUsuarios[posicao].endereco.cidade = novoValorEditar.value
+                            break
+
+                        case 'bairro':
+                            listaDeUsuarios[posicao].endereco.bairro = novoValorEditar.value
+                            break
+
+                        case 'rua':
+                            listaDeUsuarios[posicao].endereco.rua = novoValorEditar.value
+                            break
+
+                        case 'numero':
+                            listaDeUsuarios[posicao].endereco.numero = novoValorEditar.value
+                            break
+
+                        case 'complemento':
+                            listaDeUsuarios[posicao].endereco.complemento = novoValorEditar.value
+                            break
+                    }
             }
+
             localStorage.setItem("usuariosKey", JSON.stringify(listaDeUsuarios))
+            alert(`Usuário editado!`)
+
+            location.reload()
         } else {
             alert(`Este id [${idParaEditar.value}] não existe!`)
         }
@@ -195,75 +294,6 @@ function editarUsuario() {
         alert("Campo não pode ser vazio!")
     }
 }
-
-
-// ----- VALIDAÇÕES -----
-
-function validaListaUsuariosDoLocalStorage() {
-    listaDeUsuarios = JSON.parse(localStorage.getItem("usuariosKey"))
-    if (ehListaVazia(listaDeUsuarios)) {
-        listaDeUsuarios = []
-    }
-}
-/*
-function validaListaResponsaveisDoLocalStorage() {
-    listaDeResponsaveis = JSON.parse(localStorage.getItem("responsaveisKey"))
-    if(ehListaVazia(listaDeResponsaveis)){
-        listaDeResponsaveis = []
-    }
-}
-*/
-function existeId(idCheck) {
-    var retorno = false
-    listaDeUsuarios = JSON.parse(localStorage.getItem("usuariosKey"))
-    for (i = 0; i < listaDeUsuarios.length; i++) {
-        if (listaDeUsuarios[i].id == idCheck) {
-            posicao = i
-            retorno = true
-        }
-    }
-    return retorno
-}
-
-function cpfUsuarioEstaCadastrado(cpfCheck) {
-    var retorno = false
-    listaDeUsuarios = JSON.parse(localStorage.getItem("usuariosKey"))
-    for (i = 0; i < listaDeUsuarios.length; i++) {
-        if (listaDeUsuarios[i].cpf == cpfCheck) {
-            alert(`CPF [${cpfCheck}] já cadastrado!`)
-            retorno = true
-        }
-    }
-    return retorno
-}
-/*
-function cpfResponsavelEstaCadastrado(cpfCheck) {
-    var retorno = false
-    listaDeResponsaveis = JSON.parse(localStorage.getItem("responsaveisKey"))
-    for (i = 0; i < listaDeResponsaveis.length; i++) {
-        if (listaDeResponsaveis[i].cpf == cpfCheck) {
-            alert(`CPF [${cpfCheck}] já cadastrado!`)
-            retorno = true
-        }
-    }
-    return retorno
-}
-*/
-function ehInputVazio(input) {
-    var retorno = false
-    if (input == '' || input == null || input == undefined) {
-        retorno = true
-    }
-    return retorno
-}
-
-function ehListaVazia(lista) {
-    if (lista == null || lista == '' || lista == undefined) {
-        return true
-    }
-    return false
-}
-
 
 // ----- BUILDERS -----
 
@@ -291,7 +321,12 @@ function criarObjetoEndereco() {
     return objetoEndereco
 }
 
-function criarObjetoResponsavel() {
+function criarObjetoResponsavel(idDoTitular) {
+    idResp = idDoTitular
+    nomeRespInput = document.getElementById("nomeResp")
+    cpfRespInput = document.getElementById("cpfResp")
+    senhaRespInput = document.getElementById("senhaResp")
+
     var objetoResponsavel = new Responsavel(
         idResp,
         nomeRespInput.value,
@@ -304,7 +339,13 @@ function criarObjetoResponsavel() {
 }
 
 function criarObjetoEnderecoResponsavel() {
-    var objetoEnderecoResponsavel = new EnderecoResponsavel(
+    cidadeRespInput = document.getElementById("cidadeResp")
+    bairroRespInput = document.getElementById("bairroResp")
+    ruaRespInput = document.getElementById("ruaResp")
+    numeroRespInput = document.getElementById("numeroResp")
+    complementoRespInput = document.getElementById("complementoResp")
+
+    objetoEnderecoResponsavel = new EnderecoResponsavel(
         cidadeRespInput.value,
         bairroRespInput.value,
         ruaRespInput.value,
@@ -313,6 +354,126 @@ function criarObjetoEnderecoResponsavel() {
     )
 
     return objetoEnderecoResponsavel
+}
+
+function selectEndereco() {
+
+    document.getElementById("selectAtributosEndereco").innerHTML = `<select id="atributoEndereco">\n
+    <option hidden>Selecione</option>\n
+    <option value="cidade">CIDADE</option>\n
+    <option value="=bairro">BAIRRO</option>\n
+    <option value="rua">RUA</option>\n
+    <option value="numero">NÚMERO</option>\n
+    <option value="complemento">COMPLEMENTO</option>\n
+    </select>`
+
+}
+
+function inputsCadastroResponsavel() {
+    document.getElementById("inputsCadastroResponsavel").innerHTML = '<h1>CADASTRO DE RESPONSÁVEL</h1>' +
+        '<h2>NOVO RESPONSÁVEL</h2>' +
+        '<label>NOME</label><br>' +
+        '<input id="nomeResp" type="text"><br>' +
+        '<label>CPF</label><br>' +
+        '<input id="cpfResp" type="text"><br>' +
+        '<label>SENHA</label><br>' +
+        '<input id="senhaResp" type="text"><br>' +
+        '<h2>ENDEREÇO</h2>' +
+        '<label>CIDADE</label><br>' +
+        '<input id="cidadeResp" type="text"><br>' +
+        '<label>BAIRRO</label><br>' +
+        '<input id="bairroResp" type="text"><br>' +
+        '<label>RUA</label><br>' +
+        '<input id="ruaResp" type="text"><br>' +
+        '<label>NÚMERO</label><br>' +
+        '<input id="numeroResp" type="text"><br>' +
+        '<label>COMPLEMENTO</label><br>' +
+        '<input id="complementoResp" type="text"><br><br>' +
+        '<button onclick="cadastrarNovoResponsavel()">CADASTRAR</button><br><br>'
+}
+
+function HTMLnull() {
+
+    document.getElementById("htmlEndereco").innerHTML = null
+}
+
+// ----- VALIDAÇÕES -----
+
+function validaListaUsuariosDoLocalStorage() {
+    listaDeUsuarios = JSON.parse(localStorage.getItem("usuariosKey"))
+    if (ehListaVazia(listaDeUsuarios)) {
+        listaDeUsuarios = []
+    }
+}
+
+function validaListaResponsaveisDoLocalStorage() {
+    listaDeResponsaveis = JSON.parse(localStorage.getItem("responsaveisKey"))
+    if (ehListaVazia(listaDeResponsaveis)) {
+        listaDeResponsaveis = []
+    }
+}
+
+function existeId(idCheck) {
+    var retorno = false
+    listaDeUsuarios = JSON.parse(localStorage.getItem("usuariosKey"))
+    for (i = 0; i < listaDeUsuarios.length; i++) {
+        if (listaDeUsuarios[i].id == idCheck) {
+            posicao = i
+            retorno = true
+        }
+    }
+    return retorno
+}
+
+function existeNome(nomeCheck) {
+    var retorno = false
+    listaDeUsuarios = JSON.parse(localStorage.getItem("usuariosKey"))
+    for (i = 0; i < listaDeUsuarios.length; i++) {
+        if (listaDeUsuarios[i].nome == nomeCheck) {
+            posicao = i
+            retorno = true
+        }
+    }
+    return retorno
+}
+
+function existeCpf(cpfCheck) {
+    var retorno = false
+    listaDeUsuarios = JSON.parse(localStorage.getItem("usuariosKey"))
+    for (i = 0; i < listaDeUsuarios.length; i++) {
+        if (listaDeUsuarios[i].cpf == cpfCheck) {
+            posicao = i
+            retorno = true
+        }
+    }
+    return retorno
+}
+
+function existeCpfResponsavel(cpfCheck) {
+    var retorno = false
+    listaDeResponsaveis = JSON.parse(localStorage.getItem("responsaveisKey"))
+    for (i = 0; i < listaDeResponsaveis.length; i++) {
+        if (listaDeResponsaveis[i].cpf == cpfCheck) {
+            alert(`CPF [${cpfCheck}] já cadastrado!`)
+            retorno = true
+        }
+    }
+    return retorno
+}
+
+function ehInputVazio(input) {
+    var retorno = false
+    if (input == '' || input == null || input == undefined) {
+        retorno = true
+    }
+    return retorno
+}
+
+function ehListaVazia(lista) {
+    if (lista == null || lista == '' || lista == undefined) {
+        return true
+    }
+    return false
 }
 
 // ----- GERADOR DE ID ALEATÓRIO -----
